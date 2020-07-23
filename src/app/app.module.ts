7d924bdaf7fd499248c0395d3b53b25f02e41b10
Andrei Voicu <dreivoicu@gmail.com>
2020-07-23 23:18:32 +0300

    outside zone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { OUTSIDE_ZONE_PLUGINS_PROVIDER } from './outside-zone';
 import { DEFER_EVENTS_PLUGINS_PROVIDER } from './defer-events-plugin.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -7,7 +8,7 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule],
-  providers: [DEFER_EVENTS_PLUGINS_PROVIDER],
+  providers: [DEFER_EVENTS_PLUGINS_PROVIDER, OUTSIDE_ZONE_PLUGINS_PROVIDER],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
